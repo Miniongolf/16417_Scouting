8ca24c3b49722975eb72da88d4276d22b1f46705
N Cheng <nicholas.cheng@ucc.on.ca>
2023-04-21 08:11:59 -0500

    removed old code

--- index.js
+++ index.js
@@ -197,55 +197,6 @@
 // #endregion TeleOp
 
 // Gets the form link
-/*
-function generateFormLink() {
-    var link = `https://docs.google.com/forms/d/e/1FAIpQLSdoE9_KP5t3vmmbo-SqEgLw-YWyYcqDCbgHjO3L_gt_VivVYg/formResponse?usp=pp_url`;
-    
-    // #region get form values
-    matchNum = document.getElementById("matchNum").value;
-    teamNum = document.getElementById("teamNum").value;
-    startPos = document.querySelector('input[name="Starting Position"]:checked').value.replace(/ /g,"+");
-    autonTarget = document.querySelector('input[name="Auton Target"]:checked').value.replace(/ /g,"+");
-    autonCones = document.getElementById("AutonConesNum").innerHTML;
-    autonPark = document.querySelector('input[name="Auton Park"]:checked').value.replace(/ /g,"+");
-    groundCones = document.getElementById("groundConesNum").innerHTML;
-    lowCones = document.getElementById("lowConesNum").innerHTML;
-    midCones = document.getElementById("midConesNum").innerHTML;
-    highCones = document.getElementById("highConesNum").innerHTML;
-    beaconPos = document.querySelector('input[name="fieldGridButtons"]:checked').value;
-    interferes = document.querySelector('input[name="interferes"]:checked').value.replace(/ /g,"+");
-    circuit = document.querySelector('input[name="circuit"]:checked').value.replace(/ /g,"+");
-    // #endregion get form values
-
-    // #region add form values to link
-    link += `&entry.1111773089=${matchNum}`;
-    link += `&entry.1127567054=${teamNum}`;
-    link += `&entry.1007884815=${startPos}`;
-    link += `&entry.1164051351=${autonTarget}`;
-    link += `&entry.443075517=${autonCones}`;
-    link += `&entry.328230530=${autonPark}`;
-    link += `&entry.1777121679=${groundCones}`;
-    link += `&entry.44730825=${lowCones}`;
-    link += `&entry.971732476=${midCones}`;
-    link += `&entry.2039092675=${highCones}`;
-    link += `&entry.324063643=${beaconPos}`;
-    link += `&entry.1907658295=${interferes}`;
-    link += `&entry.166044511=${circuit}`;
-    link += `&submit=Submit`;
-    // #endregion add form values to link
-    
-    console.log(link);
-    open(link)
-}
-*/
-
-// Example link: 
-// https://docs.google.com/forms/d/e/1FAIpQLScfip4xqA6_sHUVIx5n9OQWzKGp4TLn_0gSyL_qSBb4_KVV0A/viewform?usp=pp_url&entry.2069581590=matchNum&entry.844732376=teamNum&entry.1470326169=Blue+far&entry.1186107460=Left&entry.1017313550=3&entry.980911135=autonCones&entry.436157894=3&entry.230575694=teleOpStrat
-
-//https://docs.google.com/forms/d/e/1FAIpQLScfip4xqA6_sHUVIx5n9OQWzKGp4TLn_0gSyL_qSBb4_KVV0A/viewform?usp=pp_url
-
-//https://docs.google.com/forms/d/e/1FAIpQLScfip4xqA6_sHUVIx5n9OQWzKGp4TLn_0gSyL_qSBb4_KVV0A/viewform?usp=pp_url&entry.1049932560=3 
-
 function genFormLink() {
     matchNum = document.getElementById("matchNum").value;
     teamNum = document.getElementById("teamNum").value;
